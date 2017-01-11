75171d2bad9dbb8a9b52e511193f4e3635bbef43
Joe <josephrimar@yahoo.com>
2017-01-11 11:56:31 -0500

    lib

--- lib/Http-services.d.ts
+++
@@ -1,9 +0,0 @@
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
-export declare class HttpService {
-    private http;
-    constructor(http: Http);
-    getData(url: string): any;
-    extractData(res: Response): {};
-}
