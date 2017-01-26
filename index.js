96a454f82d5d4b97d21d17803f90f30fcf7e2cd9
Rimar <jrimar@uspto.gov>
2017-01-26 17:46:59 -0500

    js fix

--- index.js
+++
@@ -1,2 +0,0 @@
-exports.NgValidations = require('./lib/ng2-validations.service').NgValidations
-exports.NgValidationsModule = require('./lib/main.module').NgValidationsModule
---
+++ index.ts
@@ -0,0 +1,2 @@
+export { NgValidations } from './lib/ng2-validations.service';
+export { NgValidationsModule } from './lib/main.module';
