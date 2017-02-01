772ca30b00148a23c300e96dfeee2ad90a4438bf
Rimar <jrimar@uspto.gov>
2017-02-01 17:49:16 -0500

    @types-google-libphonenumber

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ export * from './lib/main.module';
 export * from './lib/ng2-validations.service';
 export { validationDefinitions } from './lib/Samples/sample-validation-definitions';
 
-declare module "google-libphonenumber" {
-    let phone: any;
-    export = phone;
-}
+// declare module "google-libphonenumber" {
+//     let phone: any;
+//     export = phone;
+// }
