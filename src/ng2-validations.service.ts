fa762c47881327c5e0ed9cda60323d291124fe35
Joe <josephrimar@yahoo.com>
2017-03-17 16:20:21 -0400

    values array

--- src/ng2-validations.service.ts
+++ src/ng2-validations.service.ts
@@ -92,7 +92,7 @@ export class NgValidations {
 				else if (currentControl && !found && conditionsToValidate.indexOf(condition) >= 0) {
 					// Remove condition
 					console.log('Removed', condition);
-					conditionsToValidate = conditionsToValidate.filter(c => c !== condition);
+					conditionsToValidate = conditionsToValidate.filter(c => c.tests !== condition.tests || c.control !== condition.control);
 				}
 				// Check if control is required in any of the applied conditions
 				controlRequired = (<any>conditionsToValidate).findIndex(formControl => {
